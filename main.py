9375f4b9c83d1b4261f0ad99fa5cf98a2ca14ea6
baristds <101243366+baristds@users.noreply.github.com>
2023-11-17 13:09:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template, request
 import subprocess
 import os
 import time
-import boto3
+import shutil
 
 app = Flask(__name__)
 
@@ -24,8 +24,7 @@ def speed_test():
     download_speed, file_paths = measure_download_speed(urls_to_download)
     downloaded_data = sum(os.path.getsize(file_path) for file_path in file_paths) / (1024 * 1024)
 
-    # Replace with your actual bucket name and use environment variables for AWS credentials
-    uploaded_data, upload_speed = upload_to_s3(file_paths, 'cs468')
+    uploaded_data, upload_speed = save_files_locally(file_paths)
 
     return jsonify({
         'ping_result': ping_result,
@@ -67,10 +66,7 @@ def measure_download_speed(urls):
     avg_download_speed = sum(download_speeds) / len(download_speeds)
     return avg_download_speed, file_paths
 
-def upload_to_s3(file_paths, bucket_name):
-    # Use environment variables for AWS credentials
-    s3 = boto3.client('s3')
-
+def save_files_locally(file_paths):
     uploaded_data = 0
     upload_speed = 0
     for file_path in file_paths:
@@ -78,16 +74,15 @@ def upload_to_s3(file_paths, bucket_name):
             file_name = os.path.basename(file_path)
             file_size = os.path.getsize(file_path)
             start_time = time.time()
-            s3.upload_file(file_path, bucket_name, file_name)
+            destination_path = f'/local_storage/{file_name}'
+            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+            shutil.copyfile(file_path, destination_path)
             end_time = time.time()
-
-            uploaded_data += file_size / (1024 * 1024)
+            uploaded_data += file_size / (1024 * 1024)  # Convert bytes to megabytes
             upload_time = end_time - start_time
-            upload_speed += (file_size * 8) / (upload_time * 1024 * 1024)
-
+            upload_speed += (file_size * 8) / (upload_time * 1024 * 1024)  # Calculate upload speed in Mbps
         else:
             print(f"File not found: {file_path}")
-
     return uploaded_data, upload_speed
 
 if __name__ == '__main__':
